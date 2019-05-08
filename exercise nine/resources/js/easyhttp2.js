db25f98e0e291a9d1f35afcc9d391bcd8c628841
Jelle <jelletraa@gmail.com>
2019-05-08 17:27:33 +0200

    exercise 9: easyHTTP with Create delete update user with arrow functions and new

---
+++ exercise nine/resources/js/easyhttp2.js
@@ -0,0 +1,74 @@
+/** 
+* EASYHTTP Library
+* Librar for making HTTP Requests
+
+
+@version 2.0.0
+@author Jelle Traa
+@license CodeGorilla
+
+
+*/
+
+class EasyHTTP {
+    
+
+    //Make an HTTP GET Request
+    get(url) {
+        return new Promise ((resolve, reject) => {
+        fetch(url)
+        .then(res => res.json())
+        .then(data => resolve(data))
+        .catch(err => reject(err));
+        });
+
+        
+    }
+
+    //Make an HTTP Post Request
+    post(url) {
+        return new Promise ((resolve, reject) => {
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(res => res.json())
+        .then(data => resolve(data))
+        .catch(err => reject(err));
+        });
+
+    }
+    //Make an HTTP PUT Request
+    put(url, data) {
+        return new Promise ((resolve, reject) => {
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(res => res.json())
+        .then(data => resolve(data))
+        .catch(err => reject(err));
+        });
+
+    }
+    //Make an HTTP DELETE Request
+    delete(url) {
+        return new Promise ((resolve, reject) => {
+        fetch(url, {
+            method: 'DELETE',
+            headers: {
+                'Content-type': 'application/json'
+            }
+        })
+        .then(res => res.json())
+        .then(() => resolve('Resource Deleted...'))
+        .catch(err => reject(err));
+        });
+    }
+}
